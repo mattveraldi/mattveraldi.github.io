0befc1ac7b254dbde29ff1a8efe28a68b31d9db2
Matteo Veraldi <mattveraldi@gmail.com>
2023-12-15 01:20:20 +0100

    feat: first commit

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://matteoveraldi.dev/", // replace this with your deployed domain
+  author: "Matteo Veraldi",
+  desc: "My microblogging website",
+  title: "Matteo Veraldi",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/mattveraldi",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/matteo-veraldi",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  }
+];
